23c8deb351943a40613a7bfd7e78ba07cad0dd27
Shubhangi Mundhada <shubhimundhada@gmail.com>
2020-12-14 19:25:24 -0600

    Updates

---
+++ precache-manifest.f2c27f58ea222dc91daa4cab99038b0d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de707267910f774ccde594775312e0f",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "2cfe00a6681868da96bf",
+    "url": "/react-portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "62903be3b04b0f9e6d6a",
+    "url": "/react-portfolio/static/css/main.4ba38d0e.chunk.css"
+  },
+  {
+    "revision": "2cfe00a6681868da96bf",
+    "url": "/react-portfolio/static/js/2.2b2f5289.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/react-portfolio/static/js/2.2b2f5289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62903be3b04b0f9e6d6a",
+    "url": "/react-portfolio/static/js/main.d0dc058a.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "125392e286b80f6f37e6669c73b2a7c5",
+    "url": "/react-portfolio/static/media/Footer_Background.125392e2.png"
+  },
+  {
+    "revision": "e0c7c685fa581037a7e15cbe72101deb",
+    "url": "/react-portfolio/static/media/Intro_Background3.e0c7c685.png"
+  },
+  {
+    "revision": "7fceafa5b24cbe7affd4dfdca4094f97",
+    "url": "/react-portfolio/static/media/Shubhangi_Photo.7fceafa5.png"
+  },
+  {
+    "revision": "e954d1fa4d4d5757ca86ae2ec7d388c2",
+    "url": "/react-portfolio/static/media/budget_list.e954d1fa.png"
+  },
+  {
+    "revision": "9a081d166ff9f2c5b1b81f9b1847684f",
+    "url": "/react-portfolio/static/media/clipart2003747.9a081d16.png"
+  },
+  {
+    "revision": "e44e1c5f6c638b9d9151130e27bdf488",
+    "url": "/react-portfolio/static/media/insta_icon.e44e1c5f.png"
+  },
+  {
+    "revision": "972b35858efd8a9d200a7ad68ec928e1",
+    "url": "/react-portfolio/static/media/link_icon.972b3585.png"
+  },
+  {
+    "revision": "1cc7418fb1f53afd78863f497d0bd7ef",
+    "url": "/react-portfolio/static/media/proj-mars.1cc7418f.png"
+  },
+  {
+    "revision": "4c7612475835ae38b9d8a0edbec50922",
+    "url": "/react-portfolio/static/media/proj-schd.4c761247.png"
+  },
+  {
+    "revision": "0da83184ab3729f8a01626903cde80cf",
+    "url": "/react-portfolio/static/media/proj-weather.0da83184.png"
+  }
+]);
